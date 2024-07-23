6cb1c4291bf92b7a0cbeb7fd35d4185dddb05c9c
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-07-23 20:35:48 +0200

    added Get Started component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from '../style'
 import { discount, robot } from '../assets'
+import { GetStarted } from './GetStarted'
 
 const Hero = () => {
   return (
@@ -19,6 +20,9 @@ const Hero = () => {
           <span className="text-gradient">Generation</span> {" "}
           Payment Method 
           </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+             </div>
         </div>
     </section>
   )
