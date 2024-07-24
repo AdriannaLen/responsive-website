ecbc7cca3e5d8c258c81f8b0b8d29c2e283c6c08
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-07-24 14:44:44 +0200

    added mapping to section in stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,7 +3,10 @@ import styles from "../style"
 const Stats = () => {
   return (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
-      
+      {stats.map((stat) => (
+        <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
+        </div>
+        ))}
     </section>
   )
 }
