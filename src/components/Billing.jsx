783d19a2fc2f6aa44a1742ad04cc4a1c8e10b211
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-07-26 12:02:20 +0200

    latest commit

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -8,7 +8,7 @@ function Billing() {
         className="w-[100%] h-[100%] relative z[5]"/>
         <div className="absolute z=[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient">
         </div>
-        <div className="absolute z=[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient">
+        <div className="absolute z=[3] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient">
         </div>
       </div>
     </section>
