ccc69f093d78a59ae8569943dcb51e44d8f7270c
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-07-26 11:58:58 +0200

    styled divs to make gradient on the left side of the page (component Billing)

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -6,6 +6,10 @@ function Billing() {
       <div className={layout.sectionImgReverse}>
         <img src={bill} alt="billing"
         className="w-[100%] h-[100%] relative z[5]"/>
+        <div className="absolute z=[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient">
+        </div>
+        <div className="absolute z=[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient">
+        </div>
       </div>
     </section>
   )
